c477dc019d98b4ac163c4a1bb7fd5d44661a9a19
akshil savalia <30698873+AkshilSavalia@users.noreply.github.com>
2019-05-23 15:19:27 +0530

    Add files via upload

---
+++ js/js_script.js
@@ -0,0 +1,64 @@
+
+    function myFunction() {
+                            document.getElementById("love").style.fontSize = "20px";
+                            document.getElementById("love").style.color = "orange";
+                            document.getElementById("cd_1").style.borderLeft = "5px solid orange";
+                            document.getElementById("cd_1_1").style.borderTop = "5px solid orange";
+                            document.getElementById("btn-1").style.backgroundColor = "orange";
+                          }
+        
+   function myFunction2() {
+                            document.getElementById("love").style.fontSize = "15px"; 
+                            document.getElementById("cd_1").style.borderLeft = "5px solid #17aa66";
+                            document.getElementById("cd_1_1").style.borderTop = "5px solid #17aa66";
+                            document.getElementById("love").style.color = "black";
+                            document.getElementById("btn-1").style.backgroundColor = "#5cb85c";
+                          }
+        
+         function card2() {
+                            document.getElementById("love2").style.color = "orange";
+                            document.getElementById("btn-2").style.backgroundColor = "orange";
+                            document.getElementById("cd_2").style.borderLeft = "5px solid orange";
+                            document.getElementById("cd_2_1").style.borderTop = "5px solid orange";
+                            document.getElementById("love2").style.fontSize = "20px";
+                          }
+        
+       function card2_1() {
+                            document.getElementById("love2").style.fontSize = "15px";
+                            document.getElementById("cd_2").style.borderLeft = "5px solid #17aa66";
+                            document.getElementById("cd_2_1").style.borderTop = "5px solid #17aa66";
+                            document.getElementById("love2").style.color = "black";
+                            document.getElementById("btn-2").style.backgroundColor = "#5cb85c";
+                          }
+        
+         function card3() {
+                            document.getElementById("love3").style.color = "orange";
+                            document.getElementById("btn-3").style.backgroundColor = "orange";
+                            document.getElementById("cd_3").style.borderLeft = "5px solid orange";
+                            document.getElementById("cd_3_1").style.borderTop = "5px solid orange";
+                            document.getElementById("love3").style.fontSize = "20px";
+                          }
+        
+       function card3_1() {
+                            document.getElementById("love3").style.fontSize = "15px";                
+                            document.getElementById("love3").style.color = "black";
+                            document.getElementById("cd_3").style.borderLeft = "5px solid #17aa66";
+                            document.getElementById("cd_3_1").style.borderTop = "5px solid #17aa66";
+                            document.getElementById("btn-3").style.backgroundColor = "#5cb85c";
+                          }
+        
+        function card4()  {
+                            document.getElementById("love4").style.color = "orange";
+                            document.getElementById("btn-4").style.backgroundColor = "orange";
+                            document.getElementById("cd_4").style.borderLeft = "5px solid orange";
+                            document.getElementById("cd_4_1").style.borderTop = "5px solid orange";
+                            document.getElementById("love4").style.fontSize = "20px";
+                          }
+        
+       function card4_1() {
+                            document.getElementById("love4").style.fontSize = "15px";
+                            document.getElementById("cd_4").style.borderLeft = "5px solid #17aa66";
+                            document.getElementById("cd_4_1").style.borderTop = "5px solid #17aa66";                
+                            document.getElementById("love4").style.color = "black";
+                            document.getElementById("btn-4").style.backgroundColor = "#5cb85c";
+                          }
